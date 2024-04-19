8e05f0316a94210999e73f2d2474a65f2f3366ca
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-04-19 16:34:57 +0900

    01-02終わった

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    int[] map;
+    int arryIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new int[] { 1, 0, 0, 0, 0 };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    if(Input.GetKeyDown(KeyCode.UpArrow)){
+            int temp = map[arryIndex];
+            map[arryIndex] = map[arryIndex+1];
+            map[arryIndex+1]=temp;
+            arryIndex++;
+    }
+        string debugText = "";
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+        Debug.Log(debugText);
+    }
+}
