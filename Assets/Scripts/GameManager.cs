1f9359ddd17fdada31e691523c69cf74ddee2af0
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-04-23 17:12:00 +0900

    ２回目の授業内容を書いた

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,109 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    int[] map;
-    int arryIndex = 0;
+    public GameObject _playerPrefab;
+    public GameObject _boxPrefab;
+    int[,] _map;
+    GameObject[,] _field;
+
+    Vector2Int GetPlayerIndex()
+    {
+        for (int y = 0; y < _field.GetLength(0); y++)
+        {
+            for (int x = 0; x < _field.GetLength(1); x++)
+            {
+                if (_field[y, x] == null)
+                {
+                    continue;
+                }
+                if (_field[y, x].CompareTag("Player"))
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+        return new Vector2Int(-1, -1);
+    }
+
+    bool MoveNumber(string tag, Vector2Int moveFrom, Vector2Int moveTo)
+    {
+        //map範囲以外はFalseを出る
+        if (moveTo.y < 0 || moveTo.y >= _field.GetLength(0)
+            || moveTo.x < 0 || moveTo.x >= _field.GetLength(1))
+        {
+            return false;
+        }
+
+        //移動先に箱がある
+        if (_field[moveTo.y, moveTo.x] != null && _field[moveTo.y, moveTo.x].CompareTag("Box"))
+        {
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber(tag, moveTo, moveTo + velocity);
+            if (!success)
+            {
+                return false;
+            }
+        }
+
+        //自分の移動
+        _field[moveFrom.y, moveFrom.x].transform.position = new(moveTo.x, _field.GetLength(0) - moveTo.y);
+        _field[moveTo.y, moveTo.x] = _field[moveFrom.y, moveFrom.x];
+        _field[moveFrom.y, moveFrom.x] = null;
+        return true;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 1, 0, 0, 0, 0 };
+        _map = new int[,] {
+        {0,0,0,2,0},
+        {0,0,0,0,0},
+        {0,0,0,2,0},
+        {0,2,0,0,1},
+        };
+
+        _field = new GameObject[_map.GetLength(0), _map.GetLength(1)];
+
+        //マップデータによってObjを作成する
+        for (int y = 0; y < _map.GetLength(0); y++)
+        {
+            for (int x = 0; x < _map.GetLength(1); x++)
+            {
+                switch (_map[y, x])
+                {
+                    case 1:
+                        _field[y, x] = Instantiate(_playerPrefab, new Vector3(x, _map.GetLength(0) - y, 0), Quaternion.identity);
+                        break;
+                    case 2:
+                        _field[y, x] = Instantiate(_boxPrefab, new Vector3(x, _map.GetLength(0) - y, 0), Quaternion.identity);
+                        break;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    if(Input.GetKeyDown(KeyCode.UpArrow)){
-            int temp = map[arryIndex];
-            map[arryIndex] = map[arryIndex+1];
-            map[arryIndex+1]=temp;
-            arryIndex++;
-    }
-        string debugText = "";
-        for (int i = 0; i < map.Length; i++)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            debugText += map[i].ToString() + ",";
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber("Player", playerIndex, new Vector2Int(playerIndex.x + 1, playerIndex.y));
         }
-        Debug.Log(debugText);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber("Player", playerIndex, new Vector2Int(playerIndex.x - 1, playerIndex.y));
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber("Player", playerIndex, new Vector2Int(playerIndex.x, playerIndex.y - 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber("Player", playerIndex, new Vector2Int(playerIndex.x, playerIndex.y + 1));
+        }
     }
 }
